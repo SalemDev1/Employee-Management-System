b958ee8cab5700ae6723361fa3c14b7b848477f8
Mamun Salem <salemtwin2@gmail.com>
2024-10-06 23:02:29 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -8,8 +8,7 @@ class Employee {
         this.position = position;
         this.department = department;
     }
-
-    getDetails() {
+getDetails() {
         return `${this.name} works as a ${this.position} with a salary of $${this.salary} wow thats lot of cheese for this person`;
     }
 }
@@ -20,8 +19,26 @@ class Manager extends Employee {
         super(name, salary, position, department);
         this.bonus = bonus;
     }
-
-    getDetails() {
+getDetails() {
         return `${this.name} works as a ${this.position} with a salary of $${this.salary} and a bonus of $${this.bonus}, wow thats lot of cheese for this person`;
+    }
+}
+
+//Task 2-Create a Department Class
+// Department class 
+class Department {
+    constructor(name) {         //these constructors are pretty cool, not gonna lie. they because they automatically set up an object when it's created, saving you the hassle of manually assigning all its properties.
+        this.name = name;
+        this.employees = [];
+    }
+ addEmployee(employee) {
+        this.employees.push(employee);
+    }
+getDepartmentSalary() {
+        let totalSalary = 0;
+        this.employees.forEach(function(worker) {
+            totalSalary += worker.salary;
+        });
+        return totalSalary;
     }
 }
